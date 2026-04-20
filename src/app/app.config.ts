dbab3bfaad78cf5dffae76f647ac3ee4b434d5d2
gabino <jose.murielsanchez@plexus.es>
2026-04-20 14:00:47 +0200

    🔥FIX_i18n

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,30 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { mockBackendInterceptor } from './core/interceptors/mock-backend.interceptor';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideClientHydration(), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch(), withInterceptors([mockBackendInterceptor]))
-  ]
+    provideHttpClient(withFetch(), withInterceptors([mockBackendInterceptor])),
+    provideTranslateService({
+      defaultLanguage: 'es',
+
+      // 2. EL CARGADOR: Aquí se define dónde están los JSON
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/', // Ruta a la carpeta
+        suffix: '.json', // Extensión del archivo
+      }),
+    }),
+  ],
 };
